593000452b31e0f3d87ecc9f10d8840ba4032749
BosEriko <bos.eriko@gmail.com>
2019-04-20 15:52:49 +0800

    Added powerline on the install script

--- install.sh
+++ install.sh
@@ -10,3 +10,10 @@ brew install fzf && $(brew --prefix)/opt/fzf/install
 brew install ack
 brew install figlet
 brew install zsh-autosuggestions
+
+# Install powerline fonts
+git clone https://github.com/powerline/fonts.git ~/powerline-fonts
+cd ~/powerline-fonts
+./install.sh
+cd ..
+rm -rf ~/powerline-fonts
