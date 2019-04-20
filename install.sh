b72cbd48f5861b315f67d2ea8f9f99086f8c8b49
BosEriko <bos.eriko@gmail.com>
2019-04-20 16:14:09 +0800

    Moved vundle on the install script now

--- install.sh
+++ install.sh
@@ -10,21 +10,6 @@ brew install fzf
 $(brew --prefix)/opt/fzf/install
 brew install ack
 brew install figlet
-brew install zsh-autosuggestions
-
-# Install powerline fonts (https://github.com/powerline/fonts)
-git clone https://github.com/powerline/fonts.git ~/powerline-fonts
-cd ~/powerline-fonts
-./install.sh
-cd ..
-rm -rf ~/powerline-fonts
-
-# Install ZSH (https://github.com/robbyrussell/oh-my-zsh/wiki/Installing-ZSH)
-brew install zsh zsh-completions
-chsh -s /bin/zsh
-
-# Install oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Installing the dotfiles
 ## .gitconfig
@@ -42,3 +27,22 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ## .profile
 rm ~/.profile
 ln -s ~/dotfiles/profile ~/.profile
+
+# Install powerline fonts (https://github.com/powerline/fonts)
+git clone https://github.com/powerline/fonts.git ~/powerline-fonts
+cd ~/powerline-fonts
+./install.sh
+cd ..
+rm -rf ~/powerline-fonts
+
+# Install ZSH (https://github.com/robbyrussell/oh-my-zsh/wiki/Installing-ZSH)
+brew install zsh zsh-completions
+brew install zsh-autosuggestions
+chsh -s /bin/zsh
+
+# Install oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Vundle (https://github.com/VundleVim/Vundle.vim)
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
