5c64f14c251003f995c9f581dff62efcfea91888
BosEriko <bos.eriko@gmail.com>
2019-05-26 15:46:09 +0800

    Changed theme on hyper

--- hyper.js
+++ hyper.js
@@ -137,7 +137,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-afterglow"],
+  plugins: ["hyper-material-theme"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
