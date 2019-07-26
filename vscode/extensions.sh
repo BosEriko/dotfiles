2a4667c982ef650c176bf2fe5ea5e95729f22bbf
BosEriko <bos.eriko@gmail.com>
2019-07-26 10:11:56 +0800

    add a bunch of extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -2,6 +2,7 @@ code --install-extension bradgashler.htmltagwrap
 code --install-extension ChakrounAnas.turbo-console-log
 code --install-extension christian-kohler.path-intellisense
 code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension humao.rest-client
 code --install-extension karunamurti.haml
@@ -9,6 +10,12 @@ code --install-extension LaurentTreguier.vscode-simple-icons
 code --install-extension mhutchie.git-graph
 code --install-extension mikestead.dotenv
 code --install-extension mrmlnc.vscode-autoprefixer
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-ssh-explorer
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension octref.vetur
@@ -22,8 +29,10 @@ code --install-extension shyykoserhiy.vscode-spotify
 code --install-extension sianglim.slim
 code --install-extension steoates.autoimport
 code --install-extension Tyriar.sort-lines
+code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension vscodevim.vim
 code --install-extension waderyan.gitblame
 code --install-extension wayou.vscode-todo-highlight
+code --install-extension wix.vscode-import-cost
 code --install-extension xabikos.JavaScriptSnippets
 code --install-extension yahya-gilany.vscode-pomodoro
