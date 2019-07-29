f86ccb77ca82ed8087ea7011d542471b535caabd
BosEriko <bos.eriko@gmail.com>
2019-07-29 10:33:50 +0800

    add now on the list of npm packages and sorted npm list

--- install.sh
+++ install.sh
@@ -35,17 +35,18 @@ brew cleanup
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh
 
 # Install packages using NPM (https://nodejs.org/)
-npm install --global parrotsay
+npm install --global @vue/cli
+npm install --global electron
+npm install --global electron-packager
+npm install --global eslint
+npm install --global gatsby-cli
+npm install --global gtop
 npm install --global http-server
 npm install --global live-server
 npm install --global ngrok
-npm install --global gtop
-npm install --global electron
-npm install --global electron-packager
-npm install --global @vue/cli
-npm install --global gatsby-cli
+npm install --global now
+npm install --global parrotsay
 npm install --global speed-test
-npm install --global eslint
 
 # Initialize rbenv and set global ruby version (https://github.com/rbenv/rbenv)
 rbenv install 2.6.3
