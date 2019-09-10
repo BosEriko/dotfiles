b81ee61b6c744d45581cf282d66d45139ac80abd
BosEriko <bos.eriko@gmail.com>
2019-09-10 19:49:55 +0800

    fix grammar on install script

--- install.sh
+++ install.sh
@@ -102,7 +102,7 @@ rm -rf ~/karabiner-elements
 # Install nerd fonts (https://github.com/ryanoasis/nerd-fonts#font-installation)
 (cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf)
 
-# Installing the dotfiles
+# Install the dotfiles
 # =====> .gitconfig
 rm ~/.gitconfig
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
