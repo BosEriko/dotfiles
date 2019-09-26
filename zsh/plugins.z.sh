b34157db320c3f44cecdd70f23b38727f17cb271
BosEriko <bos.eriko@gmail.com>
2019-09-27 07:53:01 +0800

    remove the oh-my-zsh sourcing on antigen

--- zsh/plugins.z.sh
+++ zsh/plugins.z.sh
@@ -4,9 +4,6 @@ source $(brew --prefix)/share/antigen/antigen.zsh
 # Plugins Config
 export ZSH_PLUGINS_ALIAS_TIPS_TEXT="Has alias: "
 
-# Load the oh-my-zsh's library
-antigen use oh-my-zsh
-
 # List Antigen Plugins
 antigen bundle djui/alias-tips
 antigen bundle zsh-users/zsh-autosuggestions
