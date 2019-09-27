7681a57ed578697a0b7183ef6b1a86b0cf5942c8
BosEriko <bos.eriko@gmail.com>
2019-09-27 10:56:59 +0800

    make a cat for zsh

---
+++ cat-zsh.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cat ~/dotfiles/zsh/config.z.sh    \
+    ~/dotfiles/zsh/plugins.z.sh   \
+    ~/dotfiles/zsh/vim.z.sh       \
+    ~/dotfiles/zsh/hotkeys.z.sh   \
+    ~/dotfiles/zsh/dir.z.sh       \
+    ~/dotfiles/zsh/alias.z.sh     \
+    ~/dotfiles/zsh/docker.z.sh    \
+    ~/dotfiles/zsh/tmux.z.sh      \
+    ~/dotfiles/zsh/projects.z.sh  \
+    ~/dotfiles/zsh/ops.z.sh       \
+    ~/dotfiles/zsh/fzf.z.sh       \
+    ~/dotfiles/zsh/nvm.z.sh       \
+    ~/dotfiles/zsh/rvm.z.sh       \
+    ~/dotfiles/zsh/flutter.z.sh   \
+    ~/dotfiles/zsh/rust.z.sh      \
+    ~/dotfiles/zsh/composer.z.sh  \
+    ~/dotfiles/zsh/git.z.sh       \
+    ~/dotfiles/zsh/personal.z.sh  \
+    > zshrc-sample.text
