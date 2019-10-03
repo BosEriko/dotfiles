350674711c1d54c3c1a906b3346292a1b2c2e1f8
BosEriko <bos.eriko@gmail.com>
2019-10-03 11:58:01 +0800

    add ranger devicons on the install script

--- install.sh
+++ install.sh
@@ -60,6 +60,13 @@ php -r "unlink('composer-setup.php');"
 mv composer.phar /usr/local/bin/composer
 terminal-notifier -title 'Installing' -message 'Composer has been installed'
 
+# Install Ranger Icons (https://github.com/alexanderjeurissen/ranger_devicons)
+git clone https://github.com/alexanderjeurissen/ranger_devicons.git ~/ranger_devicons
+cd ~/ranger_devicons
+make install
+cd ~
+rm -rf ~/ranger_devicons
+
 # Install Laravel (https://laravel.com/docs/6.0)
 composer global require laravel/installer
 terminal-notifier -title 'Installing' -message 'Laravel has been installed.'
