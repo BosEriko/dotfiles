9a820bfce49af4a14ccb8c7f899328df7717e6c0
BosEriko <bos.eriko@gmail.com>
2019-10-13 13:24:39 +0800

    remove plugins and move everything to configtmux

--- tmux.conf.sh
+++ tmux.conf.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 rm ~/.tmux.conf
-cat ~/dotfiles/tmux/plugins.tmux.sh \
-    ~/dotfiles/tmux/config.tmux.sh  \
+cat ~/dotfiles/tmux/config.tmux.sh  \
     ~/dotfiles/tmux/theme.tmux.sh   \
     ~/dotfiles/tmux/hotkeys.tmux.sh \
     > ~/.tmux.conf
