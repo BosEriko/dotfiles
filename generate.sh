0db963b28fabc4249c42bcbc5c10864fdf1abc86
BosEriko <bos.eriko@gmail.com>
2019-10-16 10:40:52 +0800

    update hotkeys

--- generate.sh
+++ generate.sh
@@ -2,24 +2,24 @@
 
 # .zshrc
 rm -rf ~/.zshrc
-cat ~/dotfiles/zsh/config.z.sh    \
-    ~/dotfiles/zsh/plugins.z.sh   \
-    ~/dotfiles/zsh/vim.z.sh       \
-    ~/dotfiles/zsh/hotkeys.z.sh   \
-    ~/dotfiles/zsh/dir.z.sh       \
-    ~/dotfiles/zsh/alias.z.sh     \
-    ~/dotfiles/zsh/docker.z.sh    \
-    ~/dotfiles/zsh/tmux.z.sh      \
-    ~/dotfiles/zsh/projects.z.sh  \
-    ~/dotfiles/zsh/ops.z.sh       \
-    ~/dotfiles/zsh/fzf.z.sh       \
-    ~/dotfiles/zsh/nvm.z.sh       \
-    ~/dotfiles/zsh/rvm.z.sh       \
-    ~/dotfiles/zsh/flutter.z.sh   \
-    ~/dotfiles/zsh/rust.z.sh      \
-    ~/dotfiles/zsh/composer.z.sh  \
-    ~/dotfiles/zsh/git.z.sh       \
-    ~/dotfiles/zsh/personal.z.sh  \
+cat ~/dotfiles/zsh/config.z.sh      \
+    ~/dotfiles/zsh/plugins.z.sh     \
+    ~/dotfiles/zsh/vim.z.sh         \
+    ~/dotfiles/zsh/hotkeys.z.sh     \
+    ~/dotfiles/zsh/dir.z.sh         \
+    ~/dotfiles/zsh/alias.z.sh       \
+    ~/dotfiles/zsh/docker.z.sh      \
+    ~/dotfiles/zsh/tmux.z.sh        \
+    ~/dotfiles/zsh/projects.z.sh    \
+    ~/dotfiles/zsh/ops.z.sh         \
+    ~/dotfiles/zsh/fzf.z.sh         \
+    ~/dotfiles/zsh/nvm.z.sh         \
+    ~/dotfiles/zsh/rvm.z.sh         \
+    ~/dotfiles/zsh/flutter.z.sh     \
+    ~/dotfiles/zsh/rust.z.sh        \
+    ~/dotfiles/zsh/composer.z.sh    \
+    ~/dotfiles/zsh/git.z.sh         \
+    ~/dotfiles/zsh/personal.z.sh    \
     > ~/.zshrc
 terminal-notifier -title 'Generating' -message 'The .zshrc file has been generated.'
 
@@ -33,10 +33,10 @@ terminal-notifier -title 'Generating' -message 'The .tmux.conf file has been gen
 
 # init.vim
 rm ~/.config/nvim/init.vim || mkdir -p ~/.config/nvim
-cat ~/dotfiles/vim/plugins.vim  \
-    ~/dotfiles/vim/config.vim   \
-    ~/dotfiles/vim/theme.vim    \
-    ~/dotfiles/vim/hotkeys.vim  \
+cat ~/dotfiles/vim/plugins.vim      \
+    ~/dotfiles/vim/config.vim       \
+    ~/dotfiles/vim/theme.vim        \
+    ~/dotfiles/vim/hotkeys.vim      \
     > ~/.config/nvim/init.vim
 terminal-notifier -title 'Generating' -message 'The init.vim file has been generated.'
 
--- vim/hotkeys.vim
+++ vim/hotkeys.vim
@@ -12,30 +12,30 @@ map <right> <nop>
 
 " Remap colon to semicolon to avoid using shift too much
 map ; :
-noremap ;; ;
+nnoremap ;; ;
 
 " Control Shortcuts
-map <silent> <C-\> :NERDTreeToggle<CR>
-map <silent> <C-t> :NERDTreeFind<CR>
-map <silent> <C-h> :bp!<CR>
-map <silent> <C-l> :bn!<CR>
-map <silent> <C-x> :bd<CR>
-map <silent> <C-q> :q<CR>
-map <silent> <C-s> :w<CR>
-map <silent> <C-c> :TComment<CR>
-map <silent> <C-k> :!less ~/dotfiles/vim/hotkeys.vim<CR>
-map <silent> <C-b> :let @+ = expand("%")<CR>
-map <C-f> :CtrlSF<space>
-map <C-g> :%s/old/new/g
+nnoremap <silent> <C-\> :NERDTreeToggle<CR>
+nnoremap <silent> <C-t> :NERDTreeFind<CR>
+nnoremap <silent> <C-h> :bp!<CR>
+nnoremap <silent> <C-l> :bn!<CR>
+nnoremap <silent> <C-x> :bd<CR>
+nnoremap <silent> <C-q> :q<CR>
+nnoremap <silent> <C-s> :w<CR>
+nnoremap <silent> <C-c> :TComment<CR>
+nnoremap <silent> <C-k> :!less ~/dotfiles/vim/hotkeys.vim<CR>
+nnoremap <silent> <C-b> :let @+ = expand("%")<CR>
+nnoremap <C-f> :CtrlSF<space>
+nnoremap <C-g> :%s/old/new/g
 nnoremap <expr> <C-p> (len(system('git rev-parse')) ? ':Files' : ':GFiles --exclude-standard --others --cached')."\<cr>"
 
 " Leader Shortcuts
-nmap <leader>l :set list!<CR>
-nmap <leader>m :MinimapToggle<CR>
-nmap <leader>r :RuboCop<CR>
-nmap <leader>a :ALEToggle<CR>
-nmap <leader>w :set wrap!<CR>
-nmap <leader>g :Gblame<CR>
-nmap <leader>n :noh<CR>
+nnoremap <silent> <leader>l :set list!<CR>
+nnoremap <silent> <leader>m :MinimapToggle<CR>
+nnoremap <silent> <leader>r :RuboCop<CR>
+nnoremap <silent> <leader>a :ALEToggle<CR>
+nnoremap <silent> <leader>w :set wrap!<CR>
+nnoremap <silent> <leader>g :Gblame<CR>
+nnoremap <silent> <leader>n :noh<CR>
 set pastetoggle=<leader>p
 
