a8287026bfec44bf2e3ad7ba6dd0b09e0385b52f
BosEriko <bos.eriko@gmail.com>
2019-11-06 07:46:09 +0800

    move zsh generate script to its own folder

--- generate.sh
+++ generate.sh
@@ -1,27 +1,7 @@
 #!/bin/sh
 
 # .zshrc
-rm -rf ~/.zshrc
-cat ~/dotfiles/zsh/config.z.sh      \
-    ~/dotfiles/zsh/plugins.z.sh     \
-    ~/dotfiles/zsh/vim.z.sh         \
-    ~/dotfiles/zsh/hotkeys.z.sh     \
-    ~/dotfiles/zsh/dir.z.sh         \
-    ~/dotfiles/zsh/alias.z.sh       \
-    ~/dotfiles/zsh/docker.z.sh      \
-    ~/dotfiles/zsh/tmux.z.sh        \
-    ~/dotfiles/zsh/projects.z.sh    \
-    ~/dotfiles/zsh/ops.z.sh         \
-    ~/dotfiles/zsh/fzf.z.sh         \
-    ~/dotfiles/zsh/nvm.z.sh         \
-    ~/dotfiles/zsh/rvm.z.sh         \
-    ~/dotfiles/zsh/flutter.z.sh     \
-    ~/dotfiles/zsh/rust.z.sh        \
-    ~/dotfiles/zsh/composer.z.sh    \
-    ~/dotfiles/zsh/git.z.sh         \
-    ~/dotfiles/zsh/personal.z.sh    \
-    > ~/.zshrc
-terminal-notifier -title 'Generating' -message 'The .zshrc file has been generated.'
+~/dotfiles/zsh/generate.sh
 
 # .tmux.conf
 ~/dotfiles/tmux/generate.sh
---
+++ zsh/generate.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+rm ~/.zshrc
+cat ~/dotfiles/zsh/modules/config.z.sh      \
+    ~/dotfiles/zsh/modules/plugins.z.sh     \
+    ~/dotfiles/zsh/modules/vim.z.sh         \
+    ~/dotfiles/zsh/modules/hotkeys.z.sh     \
+    ~/dotfiles/zsh/modules/dir.z.sh         \
+    ~/dotfiles/zsh/modules/alias.z.sh       \
+    ~/dotfiles/zsh/modules/docker.z.sh      \
+    ~/dotfiles/zsh/modules/tmux.z.sh        \
+    ~/dotfiles/zsh/modules/projects.z.sh    \
+    ~/dotfiles/zsh/modules/ops.z.sh         \
+    ~/dotfiles/zsh/modules/fzf.z.sh         \
+    ~/dotfiles/zsh/modules/nvm.z.sh         \
+    ~/dotfiles/zsh/modules/rvm.z.sh         \
+    ~/dotfiles/zsh/modules/flutter.z.sh     \
+    ~/dotfiles/zsh/modules/rust.z.sh        \
+    ~/dotfiles/zsh/modules/composer.z.sh    \
+    ~/dotfiles/zsh/modules/git.z.sh         \
+    ~/dotfiles/zsh/modules/personal.z.sh    \
+    > ~/.zshrc
+terminal-notifier -title 'Generating' -message 'The .zshrc file has been generated.'
+
