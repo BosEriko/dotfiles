0db5c19e85467126f4fdd7446b0fb41b1896ab53
BosEriko <bos.eriko@gmail.com>
2019-11-13 19:15:11 +0800

    update install script to install ruby 2.6.5 instead of 2.6.3

--- install/osx/install.sh
+++ install/osx/install.sh
@@ -92,8 +92,8 @@ npm install --global tldr
 terminal-notifier -title 'Installing' -message 'NPM apps have been installed.'
 
 # Initialize rbenv and set global ruby version (https://github.com/rbenv/rbenv)
-rbenv install 2.6.3
-rbenv global 2.6.3
+rbenv install 2.6.5
+rbenv global 2.6.5
 rbenv rehash
 terminal-notifier -title 'Installing' -message 'rbenv has been initialized.'
 
