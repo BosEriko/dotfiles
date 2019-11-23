27f9a1d970219f5486bf183f2a4669451b2d0d97
BosEriko <bos.eriko@gmail.com>
2019-11-23 22:48:09 +0800

    add code as an alias of codium

--- zsh/modules/alias.z.sh
+++ zsh/modules/alias.z.sh
@@ -3,5 +3,6 @@
 
 alias vim="nvim"
 alias vi="nvim"
+alias code="codium"
 alias npm="echo 'Do you really want to use NPM instead of Yarn? (Ctrl-C to abort, or press enter to continue)' && read && npm"
 
