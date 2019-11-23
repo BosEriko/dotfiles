c0aa9c273f3b96bc3ac86823bcb5eb413fd82dc1
BosEriko <bos.eriko@gmail.com>
2019-11-23 22:19:42 +0800

    add dash to vscodium

--- vscodium/extensions.sh
+++ vscodium/extensions.sh
@@ -2,6 +2,7 @@ codium --install-extension alefragnani.Bookmarks
 codium --install-extension christian-kohler.path-intellisense
 codium --install-extension CoenraadS.bracket-pair-colorizer-2
 codium --install-extension dbaeumer.vscode-eslint
+codium --install-extension deerawan.vscode-dash
 codium --install-extension eamodio.gitlens
 codium --install-extension Equinusocio.vsc-material-theme
 codium --install-extension karunamurti.haml
@@ -11,8 +12,8 @@ codium --install-extension ms-vsliveshare.vsliveshare
 codium --install-extension octref.vetur
 codium --install-extension oderwat.indent-rainbow
 codium --install-extension rebornix.ruby
-codium --install-extension robinbentley.sass-indented
 codium --install-extension Tyriar.sort-lines
 codium --install-extension VisualStudioExptTeam.vscodeintellicode
 codium --install-extension vscodevim.vim
 codium --install-extension wayou.vscode-todo-highlight
+codium --install-extension wingrunr21.vscode-ruby
