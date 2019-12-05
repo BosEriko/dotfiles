c1daad6f6a65392f0d186f9b15217eb6fda89a97
BosEriko <bos.eriko@gmail.com>
2019-12-05 12:53:24 +0800

    add border style color

--- tmux/modules/theme.tmux.sh
+++ tmux/modules/theme.tmux.sh
@@ -3,5 +3,6 @@
 
 set -g status off
 set -wg mode-style bg='#87D787',fg='#272727'
-set -g pane-active-border-style fg='#87D787'
+set -g pane-active-border-style fg='#272727'
+set -g pane-border-style fg='#272727'
 
