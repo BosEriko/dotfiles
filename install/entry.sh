839ec45defd83debf9246e143a953a6ab498f141
BosEriko <bos.eriko@gmail.com>
2020-01-29 11:26:18 +0800

    move composer to its own shell script

--- install/entry.sh
+++ install/entry.sh
@@ -3,14 +3,7 @@
 ~/dotfiles/install/modules/brew.sh
 ~/dotfiles/install/modules/plug.sh
 ~/dotfiles/install/modules/rust.sh
-
-# Install Composer (https://getcomposer.org/doc/00-intro.md#globally)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer
-terminal-notifier -title 'Installing' -message 'Composer has been installed'
+~/dotfiles/install/modules/composer.sh
 
 # Install Ranger Icons (https://github.com/alexanderjeurissen/ranger_devicons)
 git clone https://github.com/alexanderjeurissen/ranger_devicons.git ~/ranger_devicons
