63b68b1b900b89b88f33b6b457497c4674e503d6
BosEriko <bos.eriko@gmail.com>
2020-01-30 10:56:11 +0800

    change how I'm restarting yabai on the up function

--- zsh/modules/up.z.sh
+++ zsh/modules/up.z.sh
@@ -6,7 +6,8 @@ up() {
   sh ~/dotfiles/generate.sh
   nvim +PlugInstall +qall
   terminal-notifier -title 'Installing' -message 'Vim Extensions have been installed.'
-  brew services restart yabai
+  brew services stop yabai
+  brew services start yabai
   terminal-notifier -title 'ZSH' -message 'Yabai has been restarted!'
   source ~/.zshrc
   terminal-notifier -title 'ZSH' -message 'ZSH has been restarted!'
