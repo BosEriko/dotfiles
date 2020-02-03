9dafe6a5c467daa838b4b155f46a6f205d327243
BosEriko <bos.eriko@gmail.com>
2020-02-03 17:33:09 +0800

    remove the shut down function

--- zsh/modules/down.z.sh
+++ zsh/modules/down.z.sh
@@ -6,8 +6,5 @@ down() {
   (cd ~/dotfiles && git commit -m "[Update ZSH History $(date +%s)]")
   (cd ~/dotfiles && git push origin master)
   terminal-notifier -title 'ZSH' -message 'History has been pushed.'
-  echo "Would you like to shutdown now? (Ctrl-C to abort, or press enter to continue)"
-  read
-  sudo shutdown -h now
 }
 
