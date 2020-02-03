697ebc07cf456a1e584c430f816cdf8236aee466
BosEriko <bos.eriko@gmail.com>
2020-02-03 17:54:23 +0800

    move the sync of history up

--- zsh/modules/up.z.sh
+++ zsh/modules/up.z.sh
@@ -2,6 +2,8 @@
 # ===================================================================================== [Up] ===== #
 
 up() {
+  (cd ~/dotfiles && git checkout ~/dotfiles/zsh/history)
+  terminal-notifier -title 'ZSH' -message 'History has been synced.'
   (cd ~/dotfiles && git pull)
   sh ~/dotfiles/generate.sh
   nvim +PlugInstall +qall
@@ -9,8 +11,6 @@ up() {
   brew services stop yabai
   brew services start yabai
   terminal-notifier -title 'Yabai' -message 'Yabai has been restarted!'
-  git checkout ~/dotfiles/zsh/history
-  terminal-notifier -title 'ZSH' -message 'History has been synced.'
   source ~/.zshrc
   terminal-notifier -title 'ZSH' -message 'ZSH has been restarted!'
 }
