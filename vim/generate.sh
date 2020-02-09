94037c47f27174b17b516c684bc1373ae74b5134
BosEriko <bos.eriko@gmail.com>
2020-02-09 11:48:54 +0800

    add coc.nvim file config

--- vim/generate.sh
+++ vim/generate.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
 rm ~/.config/nvim/init.vim || mkdir -p ~/.config/nvim
-cat ~/.dotfiles/vim/modules/plugins.vim      \
-    ~/.dotfiles/vim/modules/config.vim       \
-    ~/.dotfiles/vim/modules/theme.vim        \
-    ~/.dotfiles/vim/modules/hotkeys.vim      \
+cat ~/.dotfiles/vim/modules/plugins.vim \
+    ~/.dotfiles/vim/modules/config.vim  \
+    ~/.dotfiles/vim/modules/theme.vim   \
+    ~/.dotfiles/vim/modules/hotkeys.vim \
+    ~/.dotfiles/vim/modules/coc.vim     \
     > ~/.config/nvim/init.vim
 terminal-notifier -title 'Generating' -message 'The init.vim file has been generated.'
 
