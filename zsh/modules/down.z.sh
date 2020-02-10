a090abfef7b1d94c03f49bc8029a14b385a81e8d
BosEriko <bos.eriko@gmail.com>
2020-02-10 19:55:57 +0800

    add icon to history commit

--- zsh/modules/down.z.sh
+++ zsh/modules/down.z.sh
@@ -3,7 +3,7 @@
 
 down() {
   (cd ~/.dotfiles && git add ~/.dotfiles/zsh/history)
-  (cd ~/.dotfiles && git commit -m "[Update ZSH History $(date +%s)]")
+  (cd ~/.dotfiles && git commit -m ":pencil: Update ZSH History $(date +%s)")
   (cd ~/.dotfiles && git push origin master)
   terminal-notifier -title 'ZSH' -message 'History has been pushed.'
 }
