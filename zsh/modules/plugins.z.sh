c69db48befc8d3261de7c8962d9853933259a41c
BosEriko <bos.eriko@gmail.com>
2020-02-16 12:38:50 +0800

    add descriptions on antigen

--- zsh/modules/plugins.z.sh
+++ zsh/modules/plugins.z.sh
@@ -8,10 +8,10 @@ source $(brew --prefix)/share/antigen/antigen.zsh
 export ZSH_PLUGINS_ALIAS_TIPS_TEXT="Has alias: "
 
 # List Antigen Plugins
-antigen bundle djui/alias-tips
-antigen bundle zsh-users/zsh-autosuggestions
-antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle djui/alias-tips                    # An oh-my-zsh plugin to help remembering those aliases you defined once (https://github.com/djui/alias-tips)
+antigen bundle zsh-users/zsh-autosuggestions      # Fish-like autosuggestions for zsh (https://github.com/zsh-users/zsh-autosuggestions)
+antigen bundle zsh-users/zsh-completions          # Additional completion definitions for Zsh (https://github.com/zsh-users/zsh-completions)
+antigen bundle zsh-users/zsh-syntax-highlighting  # Fish shell like syntax highlighting for Zsh (https://github.com/zsh-users/zsh-syntax-highlighting)
 
 # Apply Antigen Plugins
 antigen apply
