be516a3e46d38d073b3b502f0c475b7a48cc0a9b
BosEriko <bos.eriko@gmail.com>
2020-02-18 21:22:49 +0800

    remove settings and keybindings symlink

--- install/modules/dotfiles.sh
+++ install/modules/dotfiles.sh
@@ -3,13 +3,9 @@
 # Install the dotfiles
 # =====> Stow config
 rm ~/.gitconfig ~/.hyper.js ~/.skhdrc ~/.yabairc
-(cd ~/.dotfiles && stow config)
-# =====> VSCodium Settings
 rm ~/Library/Application\ Support/VSCodium/User/settings.json
-ln -s ~/.dotfiles/vscodium/settings.json ~/Library/Application\ Support/VSCodium/User/settings.json
-# =====> VSCodium Keybindings
 rm ~/Library/Application\ Support/VSCodium/User/keybindings.json
-ln -s ~/.dotfiles/vscodium/keybindings.json ~/Library/Application\ Support/VSCodium/User/keybindings.json
+(cd ~/.dotfiles && stow config)
 # =====> Generate .zshrc, .tmux.conf and init.vim
 ~/.dotfiles/generate.sh
 # Notify the user
