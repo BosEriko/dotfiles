06ed2f9d8040b616452bc57af01ebddcd4ff7345
BosEriko <bos.eriko@gmail.com>
2020-02-19 16:40:25 +0800

    add condition to down command

--- zsh/modules/down.z.sh
+++ zsh/modules/down.z.sh
@@ -2,6 +2,9 @@
 # =================================================================================== [Down] ===== #
 
 down() {
+  (cd ~/.dotfiles && git log)
+  echo "Do you want to push your ZSH History? (Ctrl-C to abort, or press enter to continue)"
+  read
   (cd ~/.dotfiles && git add ~/.dotfiles/zsh/history)
   (cd ~/.dotfiles && git commit -m ":pencil: ZSH History #$(date +%s)")
   (cd ~/.dotfiles && git push origin master)
