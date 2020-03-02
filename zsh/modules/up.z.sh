c1c1b7df8c49f437f99da155826aa7f7d7a22159
BosEriko <bos.eriko@gmail.com>
2020-03-02 11:24:31 +0800

    add a message to vim plugclean

--- zsh/modules/up.z.sh
+++ zsh/modules/up.z.sh
@@ -7,8 +7,9 @@ up() {
   (cd ~/.dotfiles && git pull)
   sh ~/.dotfiles/generate.sh
   nvim +PlugInstall +qall
-  nvim +PlugClean +qall
   terminal-notifier -title 'Installing' -message 'Vim Extensions have been installed.'
+  nvim +PlugClean +qall
+  terminal-notifier -title 'Installing' -message 'Unused Vim Extensions have been removed.'
   brew services stop yabai
   brew services start yabai
   terminal-notifier -title 'Yabai' -message 'Yabai has been restarted!'
