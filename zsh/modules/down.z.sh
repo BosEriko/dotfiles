5e8dff9f7f59c01c9e986cbb82fa4eb932727e50
BosEriko <bos.eriko@gmail.com>
2020-03-02 13:32:52 +0800

    update down command to push the whole directory of .history

--- zsh/modules/down.z.sh
+++ zsh/modules/down.z.sh
@@ -2,12 +2,12 @@
 # =================================================================================== [Down] ===== #
 
 down() {
-  (cd ~/.history && git diff ~/.history/zsh/history)
-  echo "Do you want to push your ZSH History? (Ctrl-C to abort, or press enter to continue)"
+  (cd ~/.history && git diff)
+  echo "Do you want to push your History Repository? (Ctrl-C to abort, or press enter to continue)"
   read
-  (cd ~/.history && git add ~/.history/zsh/history)
-  (cd ~/.history && git commit -m ":pencil: ZSH History #$(date +%s)")
+  (cd ~/.history && git add .)
+  (cd ~/.history && git commit -m ":pencil: History #$(date +%s)")
   (cd ~/.history && git push origin master)
-  terminal-notifier -title 'ZSH' -message 'History has been pushed.'
+  terminal-notifier -title 'History Repository' -message 'Pushed successfully.'
 }
 
