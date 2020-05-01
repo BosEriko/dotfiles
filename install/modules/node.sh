4ccd0920b0e26b59e7f8ba82403860fc64705c53
BosEriko <bos.eriko@gmail.com>
2020-05-01 13:06:12 +0800

    add blitz and move jake to the proper line

--- install/modules/node.sh
+++ install/modules/node.sh
@@ -11,6 +11,7 @@ terminal-notifier -title 'Installing' -message 'Set Node to the LTS version.'
 
 # Install packages using NPM (https://nodejs.org/)
 yarn global add @vue/cli            # Standard Tooling for Vue.js Development (https://github.com/vuejs/vue-cli)
+yarn global add blitz               # Rails-like framework for monolithic, full-stack React apps — built on Next.js (https://github.com/blitz-js/blitz)
 yarn global add electron            # Build cross-platform desktop apps with JavaScript, HTML, and CSS (https://github.com/electron/electron)
 yarn global add electron-packager   # Customize and package your Electron app with OS-specific bundles (.app, .exe, etc.) via JS or CLI (https://github.com/electron/electron-packager)
 yarn global add eslint              # Find and fix problems in your JavaScript code (https://github.com/eslint/eslint)
@@ -20,6 +21,7 @@ yarn global add gatsby-cli          # The Gatsby command line interface (CLI) (h
 yarn global add gtop                # System monitoring dashboard for terminal (https://github.com/aksakalli/gtop)
 yarn global add http-server         # A simple zero-configuration command-line http server (https://github.com/http-party/http-server)
 yarn global add jake                # JavaScript build tool, similar to Make or Rake. Built to work with Node.js (https://github.com/jakejs/jake)
+yarn global add jake                # JavaScript build tool, similar to Make or Rake. Built to work with Node.js. (https://github.com/jakejs/jake)
 yarn global add live-server         # A simple development http server with live reload capability (https://github.com/tapio/live-server)
 yarn global add neovim              # Vim-fork focused on extensibility and usability (https://github.com/neovim/neovim)
 yarn global add ngrok               # Introspected tunnels to localhost (https://github.com/inconshreveable/ngrok)
@@ -31,7 +33,6 @@ yarn global add speed-test          # Test your internet connection speed and pi
 yarn global add tldr                # Simplified and community-driven man pages (https://github.com/tldr-pages/tldr)
 yarn global add ts-node             # TypeScript execution and REPL for node.js (https://github.com/TypeStrong/ts-node)
 yarn global add typescript          # TypeScript is a superset of JavaScript that compiles to clean JavaScript output (https://github.com/Microsoft/TypeScript)
-yarn global add jake                # JavaScript build tool, similar to Make or Rake. Built to work with Node.js.
 terminal-notifier -title 'Installing' -message 'NPM apps have been installed.'
 
 # Avoid running postinstall scripts from packages
