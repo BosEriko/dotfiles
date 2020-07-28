a0c22e53c0980f87e1dbceeb2040a9259735a50e
BosEriko <bos.eriko@gmail.com>
2020-07-28 17:55:03 +0800

    add verdacio

--- install/modules/node.sh
+++ install/modules/node.sh
@@ -32,6 +32,7 @@ yarn global add speed-test          # Test your internet connection speed and pi
 yarn global add tldr                # Simplified and community-driven man pages (https://github.com/tldr-pages/tldr)
 yarn global add ts-node             # TypeScript execution and REPL for node.js (https://github.com/TypeStrong/ts-node)
 yarn global add typescript          # TypeScript is a superset of JavaScript that compiles to clean JavaScript output (https://github.com/Microsoft/TypeScript)
+yarn global add verdaccio            # A lightweight private npm proxy registry (https://github.com/verdaccio/verdaccio)
 terminal-notifier -title 'Installing' -message 'NPM apps have been installed.'
 
 # Avoid running postinstall scripts from packages
