34cf11f92ff643888f3f7f4b66611f3bdd05d114
BosEriko <bos.eriko@gmail.com>
2020-12-16 07:29:17 +0800

    Feature: Create dr-restart alias

--- zsh/modules/docker.z.sh
+++ zsh/modules/docker.z.sh
@@ -5,6 +5,7 @@
 alias dr-web="docker-compose run web"
 alias dr-up="docker-compose up"
 alias dr-down="docker-compose down"
+alias dr-restart="docker-compose down && docker-compose up"
 alias dr-build="docker-compose down && docker-compose run web bundle install && docker-compose up --build"
 alias dr-reset="docker-compose run web rake db:schema:load"
 
