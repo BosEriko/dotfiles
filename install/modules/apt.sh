edb86112f559da6ac4b7eae522d808f1c08495ea
BosEriko <bos.eriko@gmail.com>
2022-08-11 18:54:52 +0800

    Add stow on apt.sh

--- install/modules/apt.sh
+++ install/modules/apt.sh
@@ -1,4 +1,5 @@
 #/usr/bin/env sh
 
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update -y && sudo apt upgrade -y 						# Update and upgrade Ubuntu
+sudo apt install -y stow								# GNU Stow is a symlink farm manager (https://www.gnu.org/software/stow/)
 
