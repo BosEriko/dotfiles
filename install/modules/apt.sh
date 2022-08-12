cd6e07547db21ead8fdbc86e813c913faf52dd58
BosEriko <bos.eriko@gmail.com>
2022-08-13 04:14:56 +0800

    Add install script for zsh

--- install/modules/apt.sh
+++ install/modules/apt.sh
@@ -1,4 +1,5 @@
-#/usr/bin/env sh
+#!/usr/bin/env sh
 
 sudo apt update -y && sudo apt upgrade -y               # Update and upgrade Ubuntu
 sudo apt install -y stow                                # GNU Stow is a symlink farm manager (https://www.gnu.org/software/stow/)
+sudo apt install -y zsh                                 # Zsh is a shell designed for interactive use (http://www.zsh.org/)
--- install/modules/zsh.sh
+++ install/modules/zsh.sh
@@ -1,1 +1,13 @@
-#/usr/bin/env sh
+#!/usr/bin/env sh
+
+# Install oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Installing: oh-my-zsh has been installed."
+
+# Make ZSH the default shell (https://github.com/robbyrussell/oh-my-zsh/wiki/Installing-ZSH)
+chsh -s /bin/zsh
+echo "Installing: ZSH has been set as the default shell."
+
+# Pull ZSH history (https://github.com/BosEriko/history)
+git clone https://github.com/BosEriko/history.git ~/.history
+echo "Installing: ZSH history repository has been pulled."
