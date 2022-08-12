229dc6ad21914c4797627b9fbaf6cdfd70b91be1
BosEriko <bos.eriko@gmail.com>
2022-08-13 04:02:14 +0800

    Rename rbenv.sh to ruby.sh and dotfiles.sh to config.sh

--- install/entry.sh
+++ install/entry.sh
@@ -3,10 +3,10 @@
 ~/.files/install/modules/apt.sh
 ~/.files/install/modules/plug.sh
 ~/.files/install/modules/node.sh
-~/.files/install/modules/rbenv.sh
+~/.files/install/modules/ruby.sh
 ~/.files/install/modules/gem.sh
 ~/.files/install/modules/mkdir.sh
 ~/.files/install/modules/zsh.sh
-~/.files/install/modules/dotfiles.sh
+~/.files/install/modules/config.sh
 ~/.files/install/modules/origin.sh
 ~/.files/install/modules/ssh.sh
