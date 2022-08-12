62a278f95cce42683689f5e95ac5dd8a0db6cfe3
BosEriko <bos.eriko@gmail.com>
2022-08-13 04:28:33 +0800

    add known_hosts to ssh

--- install/modules/ssh.sh
+++ install/modules/ssh.sh
@@ -3,3 +3,9 @@
 # Generate SSH Key
 ssh-keygen -C bos.eriko@gmail.com
 echo "Installing: SSH Key has been generated."
+
+# Add GitHub, GitLab and Bitbucket
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa gitlab.com >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa bitbucket.org >> ~/.ssh/known_hosts
+echo "Installing: Add GitHub, GitLab and Bitbucket to knowh_hosts"
