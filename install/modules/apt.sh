e0495754025a91232f56c185bcf1fa6b9c221be2
BosEriko <bos.eriko@gmail.com>
2022-08-12 21:13:50 +0800

    Remove neovim on the list of installation

--- install/modules/apt.sh
+++ install/modules/apt.sh
@@ -1,6 +1,4 @@
 #/usr/bin/env sh
 
-sudo apt update -y && sudo apt upgrade -y 						# Update and upgrade Ubuntu
-sudo apt install -y stow								# GNU Stow is a symlink farm manager (https://www.gnu.org/software/stow/)
-sudo apt install -y neovim                                               		# Vim-fork focused on extensibility and usability (https://github.com/neovim/neovim)
-
+sudo apt update -y && sudo apt upgrade -y               # Update and upgrade Ubuntu
+sudo apt install -y stow                                # GNU Stow is a symlink farm manager (https://www.gnu.org/software/stow/)
