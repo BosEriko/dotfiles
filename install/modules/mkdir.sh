372ffb3f8d8a4ba0515ab35bfab5d244a3aa02bb
BosEriko <bos.eriko@gmail.com>
2022-08-13 04:57:58 +0800

    Add mkdir and ruby files

--- install/modules/mkdir.sh
+++ install/modules/mkdir.sh
@@ -1,1 +1,5 @@
-#/usr/bin/env sh
+#!/usr/bin/env sh
+
+# Make the directories for Personal/Work
+mkdir -p /mnt/d/Codes/Work /mnt/d/Codes/Personal
+echo "Installing: Work and Personal folder have been created."
--- install/modules/ruby.sh
+++ install/modules/ruby.sh
@@ -1,1 +1,7 @@
-#/usr/bin/env sh
+#!/usr/bin/env sh
+
+# Initialize rbenv and set global ruby version (https://github.com/rbenv/rbenv)
+rbenv install 2.6.5
+rbenv global 2.6.5
+rbenv rehash
+echo "Installing: rbenv has been initialized."
