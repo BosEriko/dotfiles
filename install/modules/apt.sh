8551889d0937a82c2892fc560d792c5ebf6a37fb
BosEriko <bos.eriko@gmail.com>
2022-08-13 04:34:29 +0800

    Add yarn and nodejs to the install script

--- install/modules/apt.sh
+++ install/modules/apt.sh
@@ -3,3 +3,5 @@
 sudo apt update -y && sudo apt upgrade -y               # Update and upgrade Ubuntu
 sudo apt install -y stow                                # GNU Stow is a symlink farm manager (https://www.gnu.org/software/stow/)
 sudo apt install -y zsh                                 # Zsh is a shell designed for interactive use (http://www.zsh.org/)
+sudo apt install -y yarn                                # Fast, reliable, and secure dependency management (https://github.com/yarnpkg/yarn)
+sudo apt install -y nodejs                              # Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine (https://nodejs.org/en/)
