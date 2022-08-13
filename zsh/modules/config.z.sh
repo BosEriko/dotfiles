6e40d2e0167d0af3d6f793f99493cd807c86276c
BosEriko <bos.eriko@gmail.com>
2022-08-13 20:51:54 +0800

    Keep same directory for new tab/pane on windows terminal

--- zsh/modules/config.z.sh
+++ zsh/modules/config.z.sh
@@ -12,5 +12,11 @@ SAVEHIST=10000
 # Disable Compfix
 ZSH_DISABLE_COMPFIX=true
 
+# Keep same directory on new pane/tab (Windows Terminal)
+keep_current_path() {
+    printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"
+}
+precmd_functions+=(keep_current_path)
+
 # Source ZSH
 source $ZSH/oh-my-zsh.sh
