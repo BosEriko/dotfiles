461edde9153a5f3873e0c87837ec7600c06b68ce
BosEriko <bos.eriko@gmail.com>
2022-08-13 18:36:36 +0800

    add figlet

--- install/modules/apt.sh
+++ install/modules/apt.sh
@@ -10,10 +10,10 @@ sudo apt install -y rbenv                               # Groom your app’s Rub
 sudo apt install -y zsh-antigen                         # The plugin manager for zsh (https://github.com/zsh-users/antigen)
 sudo apt install -y cmatrix                             # Terminal based "The Matrix" like implementation (https://github.com/abishekvashok/cmatrix)
 sudo apt install -y cowsay                              # Let the cow talk (https://github.com/schacon/cowsay)
+sudo apt install -y figlet                              # FIGlet is a program for making large letters out of ordinary text (http://www.figlet.org/)
 
 echo "Installing: Apt apps have been installed."
 
-# brew install figlet                                               # FIGlet is a program for making large letters out of ordinary text (http://www.figlet.org/)
 # brew install fortune                                              # Fortune CLI (https://gist.github.com/zlorb/4a3eff8981fcec8ca1c7)
 # brew install git-flow                                             # Git workflow (https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow)
 # brew install github/gh/gh                                         # GitHub’s official command line tool (https://github.com/cli/cli)
--- markdown/peripherals.md
+++ markdown/peripherals.md
@@ -2,4 +2,6 @@
 - Keyboard: [Razer Huntsman Mini 60%](https://www.razer.com/gaming-keyboards/razer-huntsman-mini)
 - Mouse: [Logitech G Pro Wireless Gaming Mouse](https://www.logitechg.com/en-ph/products/gaming-mice/pro-wireless-mouse.910-005274.html)
 - Headset: [Steelseries Arctis 7 Wireless Gaming Headset](https://steelseries.com/gaming-headsets/arctis-7)
-- Monitor: N/A
+- Main Monitor: N/A
+- Left Monitor: N/A
+- Right Monitor: N/A
