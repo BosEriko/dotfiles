75c100c6bae3dd3f0dc1c827a0e4fc724cfacc7a
BosEriko <bos.eriko@gmail.com>
2022-08-14 04:15:40 +0800

    add mkdir for music/movies/youtube

--- install/modules/mkdir.sh
+++ install/modules/mkdir.sh
@@ -3,3 +3,15 @@
 # Make the directories for Personal/Work
 mkdir -p /mnt/d/Codes/Work /mnt/d/Codes/Personal
 echo "Installing: Work and Personal folder have been created."
+
+# Make the directory for your Music
+mkdir -p /mnt/d/Music
+echo "Installing: Music folder has been created."
+
+# Make the directory for your Movies
+mkdir -p /mnt/d/Movies
+echo "Installing: Movies folder has been created."
+
+# Make the directory for YouTube videos
+mkdir -p /mnt/d/Movies/YouTube
+echo "Installing: YouTube videos folder has been created."
