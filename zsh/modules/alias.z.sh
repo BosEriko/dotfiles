40f471729230c1874afd0cfc62d35c896b4fb13c
BosEriko <bos.eriko@gmail.com>
2022-08-14 04:39:17 +0800

    remove vim and vi hotkeys

--- zsh/modules/alias.z.sh
+++ zsh/modules/alias.z.sh
@@ -1,8 +1,6 @@
 
 # ================================================================================== [Alias] ===== #
 
-alias vim="code"
-alias vi="code"
 alias open="explorer.exe"
 alias copy="clip.exe"
 alias npm="echo 'Do you really want to use NPM instead of Yarn? (Ctrl-C to abort, or press enter to continue)' && read && npm"
