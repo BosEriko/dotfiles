5eda2ed397c082f292fe15c38bfb5b8d750bd983
BosEriko <bos.eriko@gmail.com>
2022-08-13 20:41:18 +0800

    add alias for explorer and clip

--- zsh/modules/alias.z.sh
+++ zsh/modules/alias.z.sh
@@ -3,4 +3,6 @@
 
 alias vim="code"
 alias vi="code"
+alias open="explorer.exe"
+alias copy="clip.exe"
 alias npm="echo 'Do you really want to use NPM instead of Yarn? (Ctrl-C to abort, or press enter to continue)' && read && npm"
