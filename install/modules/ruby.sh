3331334dfea4f600f6aa4ed1a88f7f3851b1b324
BosEriko <bos.eriko@gmail.com>
2022-08-14 01:49:23 +0800

    install ruby builds as plugin for rbenv

--- install/modules/ruby.sh
+++ install/modules/ruby.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 
+# Ruby build (https://github.com/rbenv/ruby-build)
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+
+# Update ruby build list (https://github.com/rbenv/ruby-build)
+git -C "$(rbenv root)"/plugins/ruby-build pull
+
 # Initialize rbenv and set global ruby version (https://github.com/rbenv/rbenv)
-rbenv install 2.6.5
-rbenv global 2.6.5
+rbenv install 3.1.2
+rbenv global 3.1.2
 rbenv rehash
 echo "Installing: rbenv has been initialized."
