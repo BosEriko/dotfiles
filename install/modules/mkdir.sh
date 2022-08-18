5997811b32626b539277a54945ce11b5f22ba3a7
BosEriko <bos.eriko@gmail.com>
2022-08-19 04:31:52 +0800

    create a music folder on d

--- install/modules/mkdir.sh
+++ install/modules/mkdir.sh
@@ -15,3 +15,7 @@ echo "Installing: Movies folder has been created."
 # Make the directory for YouTube videos
 mkdir -p /mnt/d/YouTube
 echo "Installing: YouTube videos folder has been created."
+
+# Make the directory for Music
+mkdir -p /mnt/d/Music
+echo "Installing: Music folder has been created."
