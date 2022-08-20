5f896468665cef21dd20bea172a1401620b55bdf
BosEriko <bos.eriko@gmail.com>
2022-08-21 02:58:26 +0800

    add environment variable

--- zsh/generate.sh
+++ zsh/generate.sh
@@ -3,7 +3,6 @@
 rm ~/.zshrc
 cat ~/.files/zsh/modules/config.z.sh            \
     ~/.files/zsh/modules/colors.z.sh            \
-    ~/.files/zsh/modules/environment.z.sh       \
     ~/.files/zsh/modules/plugins.z.sh           \
     ~/.files/zsh/modules/hotkeys.z.sh           \
     ~/.files/zsh/modules/dir.z.sh               \
@@ -19,6 +18,7 @@ cat ~/.files/zsh/modules/config.z.sh            \
     ~/.files/zsh/modules/personal.z.sh          \
     ~/.files/zsh/modules/start-session.z.sh     \
     ~/.files/zsh/modules/end-session.z.sh       \
+    ~/.files/zsh/modules/environment.z.sh       \
     ~/.files/zsh/modules/path.z.sh              \
     > ~/.zshrc
 echo "Generating: The .zshrc file has been generated."
