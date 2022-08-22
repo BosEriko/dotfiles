e555f3749b5b037f9cd16c45393d12ca66e13352
BosEriko <bos.eriko@gmail.com>
2022-08-23 00:53:06 +0800

    add title, size, seeder, leechers and downloads data

--- scripts/anime.sh
+++ scripts/anime.sh
@@ -24,6 +24,41 @@ grep -v '<i class="fa fa-comments-o"></i>' $cachedir/base.html |
   sed 's/<[^>]*>//g' |
   sed -e 's/^\s*//' -e '/^$/d' > $cachedir/data.bw
 
+# Title
+cat $cachedir/data.bw |
+  head -1 |
+  tail -1 > $cachedir/title.bw
+sed -e '1,1d' $cachedir/data.bw |
+  awk 'NR % 6 == 0' >> $cachedir/title.bw
+
+# Size
+cat $cachedir/data.bw |
+  head -2 |
+  tail -1 > $cachedir/size.bw
+sed -e '1,2d' $cachedir/data.bw |
+  awk 'NR % 6 == 0' >> $cachedir/size.bw
+
+# Seeders
+cat $cachedir/data.bw |
+  head -4 |
+  tail -1 > $cachedir/seeders.bw
+sed -e '1,4d' $cachedir/data.bw |
+  awk 'NR % 6 == 0' >> $cachedir/seeders.bw
+
+# Leechers
+cat $cachedir/data.bw |
+  head -5 |
+  tail -1 > $cachedir/leechers.bw
+sed -e '1,5d' $cachedir/data.bw |
+  awk 'NR % 6 == 0' >> $cachedir/leechers.bw
+
+# Downloads
+cat $cachedir/data.bw |
+  head -6 |
+  tail -1 > $cachedir/downloads.bw
+sed -e '1,6d' $cachedir/data.bw |
+  awk 'NR % 6 == 0' >> $cachedir/downloads.bw
+
 # Magnet links
 grep -o '<a href="magnet.*</a>' $cachedir/base.html |
   awk '{ print substr ($0, 10 ) }' |
