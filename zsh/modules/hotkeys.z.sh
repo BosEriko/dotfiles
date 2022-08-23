fdfe297c3f233bc78ca8b21a717900084e685e98
BosEriko <bos.eriko@gmail.com>
2022-08-24 04:46:30 +0800

    add hotkeys to keyboard hotkey list

--- zsh/modules/hotkeys.z.sh
+++ zsh/modules/hotkeys.z.sh
@@ -3,3 +3,4 @@
 
 bindkey -s '^o' ' ranger\n'
 bindkey -s '^z' ' fg\n'
+bindkey -s '^k' ' echo "https://boseriko-professional.notion.site/boseriko-professional/92045538d4be4360a6486d9d8d5178aa?v=af2b6b13c52a4673850844c4cf6f22b8"'
