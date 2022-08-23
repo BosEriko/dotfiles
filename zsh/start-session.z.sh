7ff290ce192f65df1ad28f66ecd57694fe1766bf
BosEriko <bos.eriko@gmail.com>
2022-08-24 05:50:00 +0800

    remove generate script on the start-session

--- zsh/start-session.z.sh
+++ zsh/start-session.z.sh
@@ -5,8 +5,6 @@ start-session() {
   (cd ~/.history && git stash && git pull)
   echo "ZSH: History has been synced."
   (cd ~/.files && git pull)
-  sh ~/.files/generate.sh
-  echo "ZSH: ZSH has been generated"
   source ~/.zshrc
   echo "ZSH: ZSH has been restarted!"
 }
