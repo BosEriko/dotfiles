13eb4d3287005e225c5cbf1412a7d14c4645b677
BosEriko <bos.eriko@gmail.com>
2022-08-25 07:18:04 +0800

    remove fzf-git install script

--- install/modules/fzf.sh
+++ install/modules/fzf.sh
@@ -4,7 +4,3 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 echo "Installing: fzf has been installed."
-
-# fzf Git (https://github.com/junegunn/fzf-git.sh)
-git clone https://github.com/junegunn/fzf-git.sh.git ~/.fzf-git
-echo "Installing: fzf-git has been installedk"
