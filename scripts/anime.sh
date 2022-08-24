a7ff7d6ad90a8c21782744d9523daf5047b1f96e
BosEriko <bos.eriko@gmail.com>
2022-08-24 19:36:48 +0800

    rename .bw to .bak

--- scripts/anime.sh
+++ scripts/anime.sh
@@ -22,67 +22,67 @@ curl -s "$baseurl/?f=2&c=1_2&q=$query&p=1" > $cachedir/base.html
 grep -v '<i class="fa fa-comments-o"></i>' $cachedir/base.html |
   sed -n -e '/<tr class="success">/,/<\/tr>/p' |
   sed 's/<[^>]*>//g' |
-  sed -e 's/^\s*//' -e '/^$/d' > $cachedir/data.bw
+  sed -e 's/^\s*//' -e '/^$/d' > $cachedir/data.bak
 
 # Title
-cat $cachedir/data.bw |
+cat $cachedir/data.bak |
   head -1 |
-  tail -1 > $cachedir/title.bw
-sed -e '1,1d' $cachedir/data.bw |
+  tail -1 > $cachedir/title.bak
+sed -e '1,1d' $cachedir/data.bak |
   awk 'NR % 6 == 0' |
-  head -19 >> $cachedir/title.bw
+  head -19 >> $cachedir/title.bak
 
-result_count=$(wc -l $cachedir/title.bw | awk '{print $1}')
+result_count=$(wc -l $cachedir/title.bak | awk '{print $1}')
 if [ "$result_count" -lt 1 ]; then
   echo "😔 No Result found. Try again 🔴"
   exit 0
 fi
 
 # Size
-cat $cachedir/data.bw |
+cat $cachedir/data.bak |
   head -2 |
-  tail -1 > $cachedir/size.bw
-sed -e '1,2d' $cachedir/data.bw |
+  tail -1 > $cachedir/size.bak
+sed -e '1,2d' $cachedir/data.bak |
   awk 'NR % 6 == 0' |
-  head -19 >> $cachedir/size.bw
+  head -19 >> $cachedir/size.bak
 
 # Seeders
-cat $cachedir/data.bw |
+cat $cachedir/data.bak |
   head -4 |
-  tail -1 > $cachedir/seeders.bw
-sed -e '1,4d' $cachedir/data.bw |
+  tail -1 > $cachedir/seeders.bak
+sed -e '1,4d' $cachedir/data.bak |
   awk 'NR % 6 == 0' |
-  head -19 >> $cachedir/seeders.bw
+  head -19 >> $cachedir/seeders.bak
 
 # Leechers
-cat $cachedir/data.bw |
+cat $cachedir/data.bak |
   head -5 |
-  tail -1 > $cachedir/leechers.bw
-sed -e '1,5d' $cachedir/data.bw |
+  tail -1 > $cachedir/leechers.bak
+sed -e '1,5d' $cachedir/data.bak |
   awk 'NR % 6 == 0' |
-  head -19 >> $cachedir/leechers.bw
+  head -19 >> $cachedir/leechers.bak
 
 # Downloads
-cat $cachedir/data.bw |
+cat $cachedir/data.bak |
   head -6 |
-  tail -1 > $cachedir/downloads.bw
-sed -e '1,6d' $cachedir/data.bw |
+  tail -1 > $cachedir/downloads.bak
+sed -e '1,6d' $cachedir/data.bak |
   awk 'NR % 6 == 0' |
-  head -19 >> $cachedir/downloads.bw
+  head -19 >> $cachedir/downloads.bak
 
 # Cleaning up some data to display
-awk '{print NR " - [Size:"$0""}' $cachedir/size.bw > $cachedir/tmp && mv $cachedir/tmp $cachedir/size.bw
-awk '{print "| S:"$0""}' $cachedir/seeders.bw > $cachedir/tmp && mv $cachedir/tmp $cachedir/seeders.bw
-awk '{print "| L:"$0"] —"}' $cachedir/leechers.bw > $cachedir/tmp && mv $cachedir/tmp $cachedir/leechers.bw
+awk '{print NR " - [Size:"$0""}' $cachedir/size.bak > $cachedir/tmp && mv $cachedir/tmp $cachedir/size.bak
+awk '{print "| S:"$0""}' $cachedir/seeders.bak > $cachedir/tmp && mv $cachedir/tmp $cachedir/seeders.bak
+awk '{print "| L:"$0"] —"}' $cachedir/leechers.bak > $cachedir/tmp && mv $cachedir/tmp $cachedir/leechers.bak
 
 # Magnet links
 grep -o '<a href="magnet.*</a>' $cachedir/base.html |
   awk '{ print substr ($0, 10 ) }' |
   awk '{ print substr( $0, 1, length($0)-40 ) }' |
-  head -20 > $cachedir/magnet.bw
+  head -20 > $cachedir/magnet.bak
 
 # Getting the line number
-echo "$(paste -d\  $cachedir/size.bw $cachedir/seeders.bw $cachedir/leechers.bw $cachedir/title.bw)"
+echo "$(paste -d\  $cachedir/size.bak $cachedir/seeders.bak $cachedir/leechers.bak $cachedir/title.bak)"
 echo "Select your stream"
 read LINE
 
@@ -93,7 +93,7 @@ fi
 echo "🔍 Searching Magnet seeds 🧲"
 
 # Get the magnet
-magnet=$(head -$LINE $cachedir/magnet.bw | tail -1)
+magnet=$(head -$LINE $cachedir/magnet.bak | tail -1)
 
 # Download the file
 webtorrent "$magnet" --out $STORAGE/Anime
--- scripts/movie.sh
+++ scripts/movie.sh
@@ -23,9 +23,9 @@ curl -s $baseurl/search/$query/1/ > $cachedir/tmp.html
 
 # Get Titles
 grep -o '<a href="/torrent/.*</a>' $cachedir/tmp.html |
-  sed 's/<[^>]*>//g' > $cachedir/titles.bw
+  sed 's/<[^>]*>//g' > $cachedir/titles.bak
 
-result_count=$(wc -l $cachedir/titles.bw | awk '{print $1}')
+result_count=$(wc -l $cachedir/titles.bak | awk '{print $1}')
 if [ "$result_count" -lt 1 ]; then
   echo "😔 No Result found. Try again 🔴"
   exit 0
@@ -33,27 +33,27 @@ fi
 
 # Seeders and Leechers
 grep -o '<td class="coll-2 seeds.*</td>\|<td class="coll-3 leeches.*</td>' $cachedir/tmp.html |
-  sed 's/<[^>]*>//g' | sed 'N;s/\n/ /' > $cachedir/seedleech.bw
+  sed 's/<[^>]*>//g' | sed 'N;s/\n/ /' > $cachedir/seedleech.bak
 
 # Size
 grep -o '<td class="coll-4 size.*</td>' $cachedir/tmp.html |
   sed 's/<span class="seeds">.*<\/span>//g' |
-  sed -e 's/<[^>]*>//g' > $cachedir/size.bw
+  sed -e 's/<[^>]*>//g' > $cachedir/size.bak
 
 # Links
 grep -E '/torrent/' $cachedir/tmp.html |
   sed -E 's#.*(/torrent/.*)/">.*/#\1#' |
-  sed 's/td>//g' > $cachedir/links.bw
+  sed 's/td>//g' > $cachedir/links.bak
 
 # Cleaning up some data to display
-sed 's/\./ /g; s/\-/ /g' $cachedir/titles.bw |
-  sed 's/[^A-Za-z0-9 ]//g' | tr -s " " > $cachedir/tmp && mv $cachedir/tmp $cachedir/titles.bw
+sed 's/\./ /g; s/\-/ /g' $cachedir/titles.bak |
+  sed 's/[^A-Za-z0-9 ]//g' | tr -s " " > $cachedir/tmp && mv $cachedir/tmp $cachedir/titles.bak
 
-awk '{print NR " - [Size:"$0" "}' $cachedir/size.bw > $cachedir/tmp && mv $cachedir/tmp $cachedir/size.bw
-awk '{print "| S:"$1 " | L:"$2"] —" }' $cachedir/seedleech.bw > $cachedir/tmp && mv $cachedir/tmp $cachedir/seedleech.bw
+awk '{print NR " - [Size:"$0" "}' $cachedir/size.bak > $cachedir/tmp && mv $cachedir/tmp $cachedir/size.bak
+awk '{print "| S:"$1 " | L:"$2"] —" }' $cachedir/seedleech.bak > $cachedir/tmp && mv $cachedir/tmp $cachedir/seedleech.bak
 
 # Getting the line number
-echo "$(paste -d\  $cachedir/size.bw $cachedir/seedleech.bw $cachedir/titles.bw)"
+echo "$(paste -d\  $cachedir/size.bak $cachedir/seedleech.bak $cachedir/titles.bak)"
 echo "Select your stream"
 read LINE
 
@@ -64,7 +64,7 @@ fi
 echo "🔍 Searching Magnet seeds 🧲"
 
 # Get the magnet
-url=$(head -n $LINE $cachedir/links.bw | tail -n +$LINE)
+url=$(head -n $LINE $cachedir/links.bak | tail -n +$LINE)
 fullURL="${baseurl}${url}/"
 curl -s $fullURL > $cachedir/tmp.html
 magnet=$(grep -Eo "magnet:\?xt=urn:btih:[a-zA-Z0-9]*" $cachedir/tmp.html | head -n 1)
