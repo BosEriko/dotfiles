d5e2828392d9ceb0a21c95b308b9975d2db95168
BosEriko <bos.eriko@gmail.com>
2022-08-30 08:16:57 +0800

    fix the whitespace removal on the clean-anime-name script

--- scripts/clean-name.sh
+++ scripts/clean-name.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(cd $STORAGE/Anime; for anime in *.mkv; do mv "$anime" "$(echo $anime | sed 's/[[][^]]*[]]//g' | sed 's/[(][^)]*[)]//g' | sed -e 's/^\s*//' -e '/^$/d')"; done)
+(cd $STORAGE/Anime; for anime in *.mkv; do mv "$anime" "$(echo $anime | sed 's/[[][^]]*[]]//g' | sed 's/[(][^)]*[)]//g'| awk '{ print substr( $0, 1, length($0)-4 ) }' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//').mkv"; done)
