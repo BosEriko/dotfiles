c9da9a8426255e3c46c4110d42cd9116b9541a3b
BosEriko <bos.eriko@gmail.com>
2022-09-02 04:23:30 +0800

    remove the whole folder of nvim

--- install/modules/config.sh
+++ install/modules/config.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 # Install the config
-# =====> Stow config
-rm ~/.config/nvim/init.lua ~/.config/nvim/init.vim || mkdir -p ~/.config/nvim
+rm -rf ~/.config/nvim/* || mkdir -p ~/.config/nvim
 rm ~/.gitconfig ~/.tmux.conf ~/.zshrc
 (cd ~/.files && stow config)
 echo "Installing: Dotfiles have been linked."
