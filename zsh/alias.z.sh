da6db6a27c3ea7fc7995d256dc86f0ecfbeeca92
BosEriko <bos.eriko@gmail.com>
2022-09-08 05:32:14 +0800

    use nvim for vi and vim

--- zsh/alias.z.sh
+++ zsh/alias.z.sh
@@ -4,3 +4,5 @@
 alias open="explorer.exe"
 alias copy="clip.exe"
 alias npm="echo 'Do you really want to use NPM instead of Yarn? (Ctrl-C to abort, or press enter to continue)' && read && npm"
+alias vi="nvim"
+alias vim="nvim"
