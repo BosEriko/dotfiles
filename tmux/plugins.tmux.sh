8c7ce14739aee2afa9f1988e9b3ccc3814d9e204
BosEriko <bos.eriko@gmail.com>
2022-09-09 10:04:49 +0800

    show bottom bar and use tokyo night on tmux

--- tmux/plugins.tmux.sh
+++ tmux/plugins.tmux.sh
@@ -2,7 +2,7 @@
 # ================================================================================ [Plugins] ===== #
 
 # Bottom Bar
-set -g @plugin "arcticicestudio/nord-tmux"
+set -g @plugin "janoamaral/tokyo-night-tmux"
 
 # Initialize the plugin manager
 run '~/.tmux/plugins/tpm/tpm'
--- tmux/theme.tmux.sh
+++ tmux/theme.tmux.sh
@@ -1,7 +1,6 @@
 
 # ================================================================================== [Theme] ===== #
 
-set -g status off
 set -wg mode-style bg='#87D787',fg='#272727'
 set -g pane-active-border-style fg='#272727'
 set -g pane-border-style fg='#272727'
