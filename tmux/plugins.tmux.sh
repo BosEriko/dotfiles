11ce862ca1373ca352543b61140df0fba5319964
BosEriko <bos.eriko@gmail.com>
2022-09-09 10:03:11 +0800

    add tpm and an example plugin

---
+++ tmux/plugins.tmux.sh
@@ -0,0 +1,8 @@
+
+# ================================================================================ [Plugins] ===== #
+
+# Bottom Bar
+set -g @plugin "arcticicestudio/nord-tmux"
+
+# Initialize the plugin manager
+run '~/.tmux/plugins/tpm/tpm'
