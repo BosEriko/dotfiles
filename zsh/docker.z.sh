3bc74c138c0c0b275bbff23d950d1d4ab9794aaa
BosEriko <bos.eriko@gmail.com>
2022-09-13 17:53:31 +0800

    add a dr:build command

--- zsh/docker.z.sh
+++ zsh/docker.z.sh
@@ -1,10 +1,11 @@
 
 # =================================================================== [Docker-compose Alias] ===== #
 
-alias dr-start="sudo service docker start"
-alias dr-web="sudo docker-compose run web"
-alias dr-up="sudo docker-compose up"
-alias dr-down="sudo docker-compose down"
-alias dr-restart="sudo docker-compose down && docker-compose up"
-alias dr-build="sudo docker-compose down && sudo docker-compose run web bundle install && sudo docker-compose up --build"
-alias dr-reset="sudo docker-compose run web rake db:schema:load"
+alias dr:start="sudo service docker start"
+alias dr:web="sudo docker-compose run web"
+alias dr:up="sudo docker-compose up"
+alias dr:down="sudo docker-compose down"
+alias dr:restart="sudo docker-compose down && docker-compose up"
+alias dr:build="sudo docker-compose build"
+alias dr:install="sudo docker-compose down && sudo docker-compose run web bundle install && sudo docker-compose up --build"
+alias dr:reset="sudo docker-compose run web rake db:schema:load"
