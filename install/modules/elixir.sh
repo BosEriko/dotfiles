2f460c7292de13df7c882f8407bf5be1da73f7b2
BosEriko <bos.eriko@gmail.com>
2022-09-13 14:02:18 +0800

    add echo information for phoenix

--- install/modules/elixir.sh
+++ install/modules/elixir.sh
@@ -2,6 +2,8 @@
 
 # Install Hex (https://hex.pm/)
 mix local.hex
+echo "Installing: Hex (Phoenix) has been installed."
 
 # Install Phoenix Generator (https://hexdocs.pm/phoenix/installation.html)
 mix archive.install hex phx_new
+echo "Installing: Phoenix Generator has been installed."
