816aa6082bdfc48bbad93bf3b9ab5b972ffb3218
BosEriko <bos.eriko@gmail.com>
2022-09-13 17:48:34 +0800

    add a start command for docker

--- zsh/docker.z.sh
+++ zsh/docker.z.sh
@@ -1,9 +1,10 @@
 
 # =================================================================== [Docker-compose Alias] ===== #
 
-alias dr-web="docker-compose run web"
-alias dr-up="docker-compose up"
-alias dr-down="docker-compose down"
-alias dr-restart="docker-compose down && docker-compose up"
-alias dr-build="docker-compose down && docker-compose run web bundle install && docker-compose up --build"
-alias dr-reset="docker-compose run web rake db:schema:load"
+alias dr-start="sudo service start docker"
+alias dr-web="sudo docker-compose run web"
+alias dr-up="sudo docker-compose up"
+alias dr-down="sudo docker-compose down"
+alias dr-restart="sudo docker-compose down && docker-compose up"
+alias dr-build="sudo docker-compose down && sudo docker-compose run web bundle install && sudo docker-compose up --build"
+alias dr-reset="sudo docker-compose run web rake db:schema:load"
