65451c0be3a47d709e80b6524a68297157dc613f
BosEriko <bos.eriko@gmail.com>
2022-09-19 12:28:07 +0800

    add a way to open glaze config fast

--- zsh/alias.z.sh
+++ zsh/alias.z.sh
@@ -7,3 +7,4 @@ alias npm="echo 'Do you really want to use NPM instead of Yarn? (Ctrl-C to abort
 alias vi="nvim"
 alias vim="nvim"
 alias v="nvim"
+alias v:glaze="nvim /mnt/c/Users/Bos/.glaze-wm/config.yaml"
