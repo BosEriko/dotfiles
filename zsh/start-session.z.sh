290bc7262f01668691d3086a1a4a0f99708f1aba
BosEriko <bos.eriko@gmail.com>
2022-09-21 14:02:38 +0800

    add an option to source zsh or not

--- zsh/start-session.z.sh
+++ zsh/start-session.z.sh
@@ -20,6 +20,12 @@ start-session() {
   # .files
   auto-pull ~/.files "Dotfiles"
   # Source ZSH
-  source ~/.zshrc
-  echo "ZSH: ZSH has been restarted!"
+  printf "${B_YELLOW}Do you want to source your ZSH config (y/n)? ${RESET}"
+  read answer
+  if [ "$answer" != "${answer#[Yy]}" ] ;then
+    source ~/.zshrc
+    echo "${GREEN}${2} ZSH has been sourced successfully.${RESET}"
+  else
+    echo "${YELLOW}${2} ZSH has not been sourced.${RESET}"
+  fi
 }
