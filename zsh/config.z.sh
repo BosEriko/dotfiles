03cfc6c1285722577e3d21d25a575b7e58a256df
BosEriko <bos.eriko@gmail.com>
2022-09-21 15:08:21 +0800

    add message to sync

--- zsh/config.z.sh
+++ zsh/config.z.sh
@@ -3,6 +3,14 @@
 
 # Load Tmux
 if [ "$TMUX" = "" ]; then
+    printf "${B_YELLOW}Before we start, do you want to sync your files first (y/n)? ${RESET}"
+    read answer
+    if [ "$answer" != "${answer#[Yy]}" ] ;then
+      start-session
+      echo "${GREEN}${2}Files has been synced.${RESET}"
+    else
+      echo "${YELLOW}${2}Skipping sync.${RESET}"
+    fi
     tmux attach-session || tmux new -s default
 fi
 
