5e2a88fabe86303a0678f4d24ae34bb30efaa3ab
BosEriko <bos.eriko@gmail.com>
2022-09-21 12:11:54 +0800

    update start-session and include password repository

--- zsh/start-session.z.sh
+++ zsh/start-session.z.sh
@@ -1,12 +1,25 @@
 
 # ========================================================================== [Start Session] ===== #
 
+auto-pull() {
+  (
+    cd $1
+    git checkout .
+    git pull
+    echo "${GREEN}${2} Repository has been synced successfully.${RESET}"
+  )
+}
+
 start-session() {
-  (cd ~/.history && git stash && git pull)
-  echo "ZSH: History has been synced."
-  (cd $STORAGE/Obsidian && git stash && git pull)
-  echo "ZSH: Obsidian has been synced."
-  (cd ~/.files && git stash && git pull)
+  # .history
+  auto-pull ~/.history "History"
+  # .history
+  auto-pull $STORAGE/Obsidian "Obsidian"
+  # password
+  auto-pull $STORAGE/Password "Password"
+  # .files
+  auto-pull ~/.files "Dotfiles"
+  # Source ZSH
   source ~/.zshrc
   echo "ZSH: ZSH has been restarted!"
 }
