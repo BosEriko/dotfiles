ccc547457ae368c639bdef851ec70843bcf7687f
BosEriko <bos.eriko@gmail.com>
2022-09-21 10:16:27 +0800

    add a way to automatically end session

--- zsh/end-session.z.sh
+++ zsh/end-session.z.sh
@@ -2,17 +2,18 @@
 # ============================================================================ [End Session] ===== #
 
 auto-push() {
-  (cd $1 && git diff)
-  printf "${B_YELLOW}Do you want to push your ${2} Repository (y/n)? ${RESET}"
-  read answer
-  if [ "$answer" != "${answer#[Yy]}" ] ;then
-    (cd $1 && git add .)
-    (cd $1 && git commit -m ":pencil: ${2} #$(date +%s)")
-    (cd $1 && git push origin master)
-    echo "${GREEN}${2} Repository: Pushed successfully.${RESET}"
-  else
-    echo "${RED}${2} Repository has not been pushed.${RESET}"
-  fi
+  (
+    cd $1
+    if [[ `git status --porcelain` ]]; then
+      echo "${CYAN}${2} Repository has been changed. Pushing.${RESET}"
+      git add .
+      git commit -m ":pencil: ${2} #$(date +%s)"
+      git push origin master
+      echo "${GREEN}${2} Repository has been pushed successfully.${RESET}"
+    else
+      echo "${YELLOW}No changes to the ${2} Repository. Skipping.${RESET}"
+    fi
+  )
 }
 
 end-session() {
