508d11d192053fd178457f0d370df48a667fe3dc
BosEriko <bos.eriko@gmail.com>
2023-03-25 15:54:06 +0800

    add a glazewm sync command

--- zsh/alias.z.sh
+++ zsh/alias.z.sh
@@ -7,6 +7,3 @@ alias npm="echo 'Do you really want to use NPM instead of Yarn? (Ctrl-C to abort
 alias vi="nvim"
 alias vim="nvim"
 alias v="nvim"
-
-# Change path accordingly if broken
-alias sync:glaze="rm -rf /mnt/c/Users/Bos/.glaze-wm/config.yaml; cp ~/.files/glazewm/config.yaml /mnt/c/Users/Bos/.glaze-wm/config.yaml"
