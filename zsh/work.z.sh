d4b5595944608f6ba0c0c5f98a642aacdf46bc18
BosEriko <bos.eriko@gmail.com>
2023-07-27 19:01:55 +0800

    add more work commands for refer scout

--- zsh/work.z.sh
+++ zsh/work.z.sh
@@ -1,12 +1,20 @@
 
 # ============================================================================= [Work Alias] ===== #
 
-alias work:saturn:start="sudo service postgresql start && sudo service redis-server start && bin/dev"
+SATURN_URL="localhost:3000"
+
+alias work:saturn:start="sudo service postgresql start && sudo service redis-server start && bin/dev && wslview ${SATURN_URL}"
 alias work:saturn:console="rails c"
 alias work:saturn:sidekiq="sidekiq"
 alias work:saturn:install="bundle install && yarn install"
 
-alias work:refer:start="sudo service postgresql start && sudo service redis-server start && echo 'ReferScout start script.'"
+REFERSCOUT_URL="http://r-scout.lvh.me:3000/login"
+
+alias work:refer:start="sudo service postgresql start && sudo service redis-server start && bin/rails server && wslview ${REFERSCOUT_URL}"
+alias work:refer:console="rails c"
+alias work:refer:sidekiq="sidekiq"
+alias work:refer:webpack="bin/webpack-dev-server"
+alias work:refer:install="bundle install && yarn install"
 
 work:help() {
   (cd ~; figlet 'Work' | lolcat && echo -e $WORK_HELP_MESSAGE;)
