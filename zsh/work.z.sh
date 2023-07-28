4024d88b713e36854914ef486c5c3959b56b6c2c
BosEriko <bos.eriko@gmail.com>
2023-07-28 13:25:55 +0800

    update work commands to go to path

--- zsh/work.z.sh
+++ zsh/work.z.sh
@@ -2,29 +2,31 @@
 # ============================================================================= [Work Alias] ===== #
 
 SATURN_URL="localhost:3000"
+SATURN_PATH="~/Documents/Codes/Work/resonate/saturn"
 
-alias work:saturn:start="sudo service postgresql start && sudo service redis-server start && bin/dev && wslview ${SATURN_URL}"
-alias work:saturn:console="rails c"
-alias work:saturn:sidekiq="sidekiq"
-alias work:saturn:install="bundle install && yarn install"
+alias work:saturn:start="cd ${SATURN_PATH} && sudo service postgresql start && sudo service redis-server start && bin/dev && wslview ${SATURN_URL}"
+alias work:saturn:console="cd ${SATURN_PATH} && rails c"
+alias work:saturn:sidekiq="cd ${SATURN_PATH} && bundle exec sidekiq"
+alias work:saturn:install="cd ${SATURN_PATH} && bundle install && yarn install"
 
-alias work:saturn:console:staging="heroku run rails c --app saturncms-staging"
-alias work:saturn:console:qa="heroku run rails c --app apmc-allrs-qa"
-alias work:saturn:console:prod="heroku run rails c --app apmc-allrs-prod"
-alias work:saturn:console:pre-prod="heroku run rails c --app apmc-allrs-pre-prod"
+alias work:saturn:console:staging="cd ${SATURN_PATH} && heroku run rails c --app saturncms-staging"
+alias work:saturn:console:qa="cd ${SATURN_PATH} && heroku run rails c --app apmc-allrs-qa"
+alias work:saturn:console:prod="cd ${SATURN_PATH} && heroku run rails c --app apmc-allrs-prod"
+alias work:saturn:console:pre-prod="cd ${SATURN_PATH} && heroku run rails c --app apmc-allrs-pre-prod"
 
-alias work:saturn:logs:staging="heroku logs --tail --app saturncms-staging"
-alias work:saturn:logs:qa="heroku logs --tail --app apmc-allrs-qa"
-alias work:saturn:logs:prod="heroku logs --tail --app apmc-allrs-prod"
-alias work:saturn:logs:pre-prod="heroku logs --tail --app apmc-allrs-pre-prod"
+alias work:saturn:logs:staging="cd ${SATURN_PATH} && heroku logs --tail --app saturncms-staging"
+alias work:saturn:logs:qa="cd ${SATURN_PATH} && heroku logs --tail --app apmc-allrs-qa"
+alias work:saturn:logs:prod="cd ${SATURN_PATH} && heroku logs --tail --app apmc-allrs-prod"
+alias work:saturn:logs:pre-prod="cd ${SATURN_PATH} && heroku logs --tail --app apmc-allrs-pre-prod"
 
 REFERSCOUT_URL="http://r-scout.lvh.me:3000/login"
+REFERSCOUT_PATH="~/Documents/Codes/Work/referscout"
 
-alias work:refer:start="sudo service postgresql start && sudo service redis-server start && bin/rails server && wslview ${REFERSCOUT_URL}"
-alias work:refer:console="rails c"
-alias work:refer:sidekiq="sidekiq"
-alias work:refer:webpack="bin/webpack-dev-server"
-alias work:refer:install="bundle install && yarn install"
+alias work:refer:start="cd ${REFERSCOUT_PATH} && sudo service postgresql start && sudo service redis-server start && bin/rails server && wslview ${REFERSCOUT_URL}"
+alias work:refer:console="cd ${REFERSCOUT_PATH} && rails c"
+alias work:refer:sidekiq="cd ${REFERSCOUT_PATH} && bundle exec sidekiq"
+alias work:refer:webpack="cd ${REFERSCOUT_PATH} && bin/webpack-dev-server"
+alias work:refer:install="cd ${REFERSCOUT_PATH} && bundle install && yarn install"
 
 work:help() {
   (cd ~; figlet 'Work' | lolcat && echo -e $WORK_HELP_MESSAGE;)
@@ -55,5 +57,9 @@ ${RESET}
     saturn:logs:pre-prod        Open Saturn logs (Pre-prod)
 
     refer:start                 Start ReferScout
+    saturn:console              Start ReferScout console
+    saturn:sidekiq              Start ReferScout sidekiq
+    saturn:webpack              Start ReferScout webpack
+    saturn:install              Install Saturn dependencies
 
 "
