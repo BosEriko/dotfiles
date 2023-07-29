043a1e3e3a9b1ccb419b931bbccaa35d55e1f36f
BosEriko <bos.eriko@gmail.com>
2023-07-29 20:59:09 +0800

    update tmux script to not use alias

--- tmux/workspace/referscout.tmux.sh
+++ tmux/workspace/referscout.tmux.sh
@@ -1,19 +1,32 @@
 rename-session referscout
 kill-window -a
 kill-pane -a
+
+send "cd ~/Documents/Codes/Work/referscout" C-m
+send "echo ${PASSWORD} | sudo -S service postgresql start" C-m
+send "echo ${PASSWORD} | sudo -S service redis-server start" C-m
+send "rails db:migrate RAILS_ENV=development" C-m
+send "bundle install" C-m
+send "yarn install" C-m
+send "wslview http://r-scout.lvh.me:3000/login" C-m
+
 rename-window editor
-send "v" C-m
+send "vim" C-m
 split-window -f
 send "(cd ~; clear; figlet 'ReferScout' -f big | lolcat;)" C-m
 select-pane -t 1
+
 new-window -n process
-send "work:referscout:console" C-m
+send "rails console" C-m
 split-window -h
-send "work:referscout:start" C-m
+send "bin/rails server" C-m
 split-window -f
-send "work:referscout:sidekiq" C-m
+send "bundle exec sidekiq" C-m
 split-window -h
-send "work:referscout:webpack" C-m
+send "bin/webpack-dev-server" C-m
 select-pane -t 1
 resize-pane -Z
+
+new-window -n notes
+
 select-window -t editor
--- tmux/workspace/saturn.tmux.sh
+++ tmux/workspace/saturn.tmux.sh
@@ -1,17 +1,30 @@
 rename-session saturn
 kill-window -a
 kill-pane -a
+
+send "cd ~/Documents/Codes/Work/resonate/saturn" C-m
+send "echo ${PASSWORD} | sudo -S service postgresql start" C-m
+send "echo ${PASSWORD} | sudo -S service redis-server start" C-m
+send "rails db:migrate RAILS_ENV=development" C-m
+send "bundle install" C-m
+send "yarn install" C-m
+send "wslview http://localhost:3000" C-m
+
 rename-window editor
-send "v" C-m
+send "vim" C-m
 split-window -f
 send "(cd ~; clear; figlet 'Saturn' -f big | lolcat;)" C-m
 select-pane -t 1
+
 new-window -n process
-send "work:saturn:console" C-m
+send "rails console" C-m
 split-window -f
-send "work:saturn:start" C-m
+send "bin/dev" C-m
 split-window -h
-send "work:saturn:sidekiq" C-m
+send "bundle exec sidekiq" C-m
 select-pane -t 1
 resize-pane -Z
+
+new-window -n notes
+
 select-window -t editor
