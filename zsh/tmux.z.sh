7dd5d690d238d19a22153161c55cd46a5d6ab002
BosEriko <bos.eriko@gmail.com>
2023-07-30 20:20:29 +0800

    update tmux script

--- zsh/tmux.z.sh
+++ zsh/tmux.z.sh
@@ -1,8 +1,8 @@
 
 # ============================================================================= [Tmux alias] ===== #
 
-alias t-new="tmux new-session"
-alias t-list="tmux list-sessions"
-alias t-last="tmux attach-session"
-alias t-clean="tmux kill-session -a && tmux kill-pane -a && tmux kill-window -a"
-alias t-quit="tmux kill-session"
+alias tmux:new="tmux new-session"
+alias tmux:list="tmux list-sessions"
+alias tmux:last="tmux attach-session"
+alias tmux:clean="tmux kill-session -a && tmux kill-pane -a && tmux kill-window -a"
+alias tmux:quit="tmux kill-session"
