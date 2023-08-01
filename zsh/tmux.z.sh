c34fdc91b2c8da6e62ab4dfadd64875267ed5e9a
BosEriko <bos.eriko@gmail.com>
2023-08-01 21:47:31 +0800

    add redirect to root directory and add clear to tmux:clean

--- zsh/tmux.z.sh
+++ zsh/tmux.z.sh
@@ -4,5 +4,5 @@
 alias tmux:new="tmux new-session"
 alias tmux:list="tmux list-sessions"
 alias tmux:last="tmux attach-session"
-alias tmux:clean="tmux kill-session -a && tmux kill-pane -a && tmux kill-window -a"
+alias tmux:clean="tmux kill-session -a && tmux kill-pane -a && tmux kill-window -a && cd ~ && clear"
 alias tmux:quit="tmux kill-session"
