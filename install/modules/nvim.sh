25665509c9e1d46a23ead124f194cc2edda9ca6d
BosEriko <bos.eriko@gmail.com>
2023-08-07 12:14:57 +0800

    remove xclip after installing neovim

--- install/modules/nvim.sh
+++ install/modules/nvim.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # Neovim is a project that seeks to aggressively refactor Vim (https://github.com/neovim/neovim)
-sudo apt remove -y neovim
+sudo apt-get remove -y neovim
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install -y neovim
+sudo apt-get remove -y xclip
