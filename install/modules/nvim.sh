c6b2453d4a4d478092b25f1361da93cebf3b4e9a
BosEriko <bos.eriko@gmail.com>
2023-08-07 12:01:49 +0800

    fix update script for nvim

--- install/modules/nvim.sh
+++ install/modules/nvim.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Neovim is a project that seeks to aggressively refactor Vim (https://github.com/neovim/neovim)
-sudo apt remove neovim -y
+sudo apt remove -y neovim
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install -y neovim
