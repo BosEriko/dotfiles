e0941dcd5d59230a12ae316f8476a1a1ade33cad
BosEriko <bos.eriko@gmail.com>
2023-09-03 19:56:13 +0800

    add alias for vscode

--- zsh/alias.z.sh
+++ zsh/alias.z.sh
@@ -6,6 +6,8 @@ alias copy="clip.exe"
 alias npm="echo 'Do you really want to use NPM instead of Yarn? (Ctrl-C to abort, or press enter to continue)' && read && npm"
 alias sudo="echo $PASSWORD | sudo -S "
 
+alias vsc="code"
+alias vs="code"
 alias vim="nvim"
 alias vi="nvim"
 alias v="nvim"
