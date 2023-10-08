e374a7b608348adcc5b8b58270d139bfc81ba7b1
BosEriko <bos.eriko@gmail.com>
2023-10-08 12:42:11 +0800

    add example kuru-studio-social tmux config

---
+++ tmux/workspace/kuru-studio-social.tmux.sh
@@ -0,0 +1,32 @@
+rename-session kuru-studio-social
+kill-session -a
+kill-window -a
+kill-pane -a
+
+rename-window editor
+send "vim" C-m
+split-window -f
+send "(cd ~; clear; figlet 'KSS Web' -f big | lolcat;)" C-m
+send "(cd ~; clear; figlet 'KSS Server' -f big | lolcat;)" C-m
+select-pane -t 1
+
+# new-window -n process
+# send "rails console" C-m
+# split-window -h
+# send "bin/rails server -p 3000" C-m
+# split-window -f
+# send "yarn dev:js" C-m
+# split-window -h
+# send "yarn build:css --watch" C-m
+# split-window -h
+# send "bundle exec sidekiq" C-m
+# select-pane -t 3
+# split-window -h
+# send "yarn dev:jsx" C-m
+# select-pane -t 1
+# resize-pane -Z
+
+# new-window -n notes
+# send "joplin" C-m
+
+select-window -t editor
