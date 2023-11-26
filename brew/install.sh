4d2d18f6792c619040f4b72bc7b04cb6a31063e3
BosEriko <bos.eriko@gmail.com>
2023-11-26 11:05:55 +0800

    add structure to brew

--- brew/install.sh
+++ brew/install.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+# Add extra bucket
+# scoop bucket add extras
+# scoop bucket add main
+
 # Install Applications
-brew install git              # git
+brew install git                # git
+# scoop install neovim          # Neovim
+# scoop install autohotkey      # AutoHotkey
+# scoop install obsidian        # Obsidian
+# scoop install taskbarx        # TaskbarX
+# scoop install keepassxc       # KeePassXC
+# scoop install powertoys       # PowerToys
+# scoop install vscodium        # VSCodium
+# scoop install gimp            # Gimp
 
 # Install Extras
-brew install --cask vivaldi   # Vivaldi Browser
+# scoop install qutebrowser     # qutebrowser Browser
+# scoop install min             # min Browser
+brew install --cask vivaldi     # Vivaldi Browser
+# scoop install obs-studio      # OBS
+# scoop install autohotkey      # AutoHotkey
+
+# Install Fira Mono
+# scoop bucket add nerd-fonts
+# scoop install FiraMono-NF
