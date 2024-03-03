2e9d7590d42c140081ad4048bcb5a67cc787ccc6
Bos Eriko Reyes <bos.eriko@gmail.com>
2024-03-02 22:41:25 -0800

    add blender to scoop sh

--- scoop/install.sh
+++ scoop/install.sh
@@ -5,20 +5,21 @@ scoop bucket add extras
 scoop bucket add main
 
 # Install Applications
-scoop install neovim          # Neovim
-scoop install obsidian        # Obsidian
-scoop install keepassxc       # KeePassXC
-scoop install vscodium        # VSCodium
-scoop install gimp            # Gimp
-scoop install autohotkey      # AutoHotkey
-scoop install taskbarx        # TaskbarX
-scoop install powertoys       # PowerToys
+scoop install neovim                    # Neovim
+scoop install obsidian                  # Obsidian
+scoop install keepassxc                 # KeePassXC
+scoop install vscodium                  # VSCodium
+scoop install gimp                      # Gimp
+scoop install autohotkey                # AutoHotkey
+scoop install taskbarx                  # TaskbarX
+scoop install powertoys                 # PowerToys
 
 # Install Extras
-scoop install qutebrowser     # qutebrowser Browser
-scoop install min             # min Browser
-scoop install vivaldi         # Vivaldi Browser
-scoop install obs-studio      # OBS
+scoop install qutebrowser               # qutebrowser Browser
+scoop install min                       # min Browser
+scoop install vivaldi                   # Vivaldi Browser
+scoop install obs-studio                # OBS
+scoop install extras/blender-launcher   # Blender Launcher
 
 # Install Fira Mono
 scoop bucket add nerd-fonts
