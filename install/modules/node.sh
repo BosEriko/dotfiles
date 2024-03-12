24aac2c19fb08c0c9aebcd4d6b079b90583e2568
BosEriko <bos.eriko@gmail.com>
2024-03-12 11:33:25 +0800

    remove unwanted packages on yarn

--- install/modules/node.sh
+++ install/modules/node.sh
@@ -9,14 +9,9 @@ echo "Installing: fnm has been installed."
 
 # Install packages using NPM (https://nodejs.org/)
 yarn global add ngrok                       # Introspected tunnels to localhost (https://github.com/inconshreveable/ngrok)
-yarn global add nodemon                     # Monitor for any changes in your node.js application and automatically restart the server - perfect for development (https://github.com/remy/nodemon)
-yarn global add speed-test                  # Test your internet connection speed and ping using speedtest.net from the CLI (https://github.com/sindresorhus/speed-test)
-yarn global add tldr                        # Simplified and community-driven man pages (https://github.com/tldr-pages/tldr)
-yarn global add ts-node                     # TypeScript execution and REPL for node.js (https://github.com/TypeStrong/ts-node)
 yarn global add typescript                  # TypeScript is a superset of JavaScript that compiles to clean JavaScript output (https://github.com/Microsoft/TypeScript)
 yarn global add typescript-language-server  # Language Server Protocol implementation for TypeScript wrapping tsserver (https://github.com/typescript-language-server/typescript-language-server)
 yarn global add webtorrent-cli              # WebTorrent is the first BitTorrent client that works in the browser (https://github.com/webtorrent/webtorrent-cli)
-yarn global add vercel                      # The easiest way to deploy websites (https://github.com/zeit/now)
 yarn global add neovim                      # Neovim is a project that seeks to aggressively refactor Vim (https://github.com/neovim/neovim)
 echo "Installing: NPM apps have been installed."
 
