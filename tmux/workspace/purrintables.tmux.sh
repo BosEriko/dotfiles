198be7e2bc41bb3099953a3184cb15e32e013760
BosEriko <bos.eriko@gmail.com>
2024-04-22 19:47:34 +0800

    add ks server to purrintables codebase

--- tmux/workspace/purrintables.tmux.sh
+++ tmux/workspace/purrintables.tmux.sh
@@ -6,10 +6,24 @@ kill-pane -a
 rename-window editor
 send "vim" C-m
 split-window -f
+send "cd ~/Documents/Codes/Work/kuru-studio/purrintables" C-m
 send "(cd ~; clear; figlet 'Purrintables' -f big | lolcat;)" C-m
+split-window -h
+send "cd ~/Documents/Codes/Work/kuru-studio/kuru-studio-social/server" C-m
+send "(cd ~; clear; figlet 'KSS Server' -f big | lolcat;)" C-m
 select-pane -t 1
 
 new-window -n process
+send "cd ~/Documents/Codes/Work/kuru-studio/kuru-studio-social/server" C-m
+send "docker-compose up" C-m
+split-window -f
+send "cd ~/Documents/Codes/Work/kuru-studio/purrintables" C-m
 send "yarn develop" C-m
+select-pane -t 1
+split-window -h
+send "cd ~/Documents/Codes/Work/kuru-studio/kuru-studio-social/server" C-m
+send "docker-compose run web rails console" C-m
+select-pane -t 2
+resize-pane -Z
 
 select-window -t editor
