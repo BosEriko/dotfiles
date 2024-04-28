3e94bb0cb142638fb7411cead88f44a3d2c8112a
BosEriko <bos.eriko@gmail.com>
2024-04-28 16:16:01 +0800

    add docker prune to tmux:clean

--- zsh/tmux.z.sh
+++ zsh/tmux.z.sh
@@ -4,5 +4,15 @@
 alias tmux:new="tmux new-session"
 alias tmux:list="tmux list-sessions"
 alias tmux:last="tmux attach-session"
-alias tmux:clean="tmux kill-session -a && tmux kill-pane -a && tmux kill-window -a && cd ~ && clear"
 alias tmux:quit="tmux kill-session"
+
+tmux:clean() {
+  tmux kill-session -a
+  tmux kill-pane -a
+  tmux kill-window -a
+  sudo docker network prune -f
+  sudo docker stop $(docker ps -aq)
+  sudo docker rm $(docker ps -aq)
+  cd ~
+  clear
+}
