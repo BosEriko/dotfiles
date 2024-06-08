d09a4dca05836373b87f743489c3ff20f1ad52d6
BosEriko <bos.eriko@gmail.com>
2024-06-08 08:48:59 +0800

    add kuru-studio-panel to workspaces

--- zsh/work.z.sh
+++ zsh/work.z.sh
@@ -11,20 +11,24 @@ workspace() {
   clear
   (cd ~; figlet 'When you enjoy what you do, work becomes play.' -f small | lolcat;)
   echo "Select Workspace:"
-  echo "  1. Kuru Studio Network"
-  echo "  2. ReferScout"
+  echo "  1. Kuru Studio Panel"
+  echo "  2. Kuru Studio Network"
   echo "  3. Purrintables"
-  echo "Please choose between 1 to 3:"
+  echo "  4. ReferScout"
+  echo "Please choose between 1 to 4:"
   read OPTION
   case "$OPTION" in
   "1")
-    workspace:kuru-studio-network
+    workspace:kuru-studio-panel
     ;;
   "2")
-    workspace:referscout
+    workspace:kuru-studio-network
     ;;
   "3")
     workspace:purrintables
+    ;;
+  "4")
+    workspace:referscout
     ;;
   *)
     echo "Invalid selection."
@@ -32,8 +36,8 @@ workspace() {
   esac
 }
 
-workspace:kuru-studio-network() {
-  cd ~/Documents/Codes/Work/kuru-studio/kuru-studio-network/web
+workspace:kuru-studio-panel() {
+  cd ~/Documents/Codes/Work/kuru-studio/kuru-studio-panel
   if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
     if [[ `git status --porcelain` ]]; then
       git stash
@@ -58,6 +62,11 @@ workspace:kuru-studio-network() {
     sudo docker-compose run web bundle install
     sudo docker-compose build
   fi
+  wslview http://localhost:3000
+  tmux source-file ~/.files/tmux/workspace/kuru-studio-panel.tmux.sh
+}
+
+workspace:kuru-studio-network() {
   cd ~/Documents/Codes/Work/kuru-studio/kuru-studio-network
   if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
     if [[ `git status --porcelain` ]]; then
@@ -67,9 +76,40 @@ workspace:kuru-studio-network() {
     else
       git pull origin master
     fi
+    yarn install
+  fi
+  cd ~/Documents/Codes/Work/kuru-studio/kuru-studio-server
+  if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
+    if [[ `git status --porcelain` ]]; then
+      git stash
+      git pull origin master
+      git stash apply
+    else
+      git pull origin master
+    fi
+    sudo docker-compose run web rails db:migrate
+    sudo docker-compose down
+    sudo docker-compose run web bundle install
+    sudo docker-compose build
   fi
   wslview http://localhost:3000
   tmux source-file ~/.files/tmux/workspace/kuru-studio-network.tmux.sh
+}
+
+workspace:purrintables() {
+  cd ~/Documents/Codes/Work/kuru-studio/purrintables
+  if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
+    if [[ `git status --porcelain` ]]; then
+      git stash
+      git pull origin master
+      git stash apply
+    else
+      git pull origin master
+    fi
+    yarn install
+  fi
+  wslview http://localhost:3000
+  tmux source-file ~/.files/tmux/workspace/purrintables.tmux.sh
 }
 
 workspace:referscout() {
@@ -88,21 +128,5 @@ workspace:referscout() {
   fi
   wslview https://r-scout.localhost.dev:3001/login
   tmux source-file ~/.files/tmux/workspace/referscout.tmux.sh
-}
-
-workspace:purrintables() {
-  cd ~/Documents/Codes/Work/kuru-studio/purrintables
-  if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
-    if [[ `git status --porcelain` ]]; then
-      git stash
-      git pull origin master
-      git stash apply
-    else
-      git pull origin master
-    fi
-    yarn install
-  fi
-  wslview http://localhost:3000
-  tmux source-file ~/.files/tmux/workspace/purrintables.tmux.sh
 }
 
